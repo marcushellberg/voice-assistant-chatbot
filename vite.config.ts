c14317c341feb9b305c181bb7fa26b5e0b214391
Marcus Hellberg <marcus@vaadin.com>
2024-12-05 16:15:12 -0800

    remove tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
-  css: {
-    postcss: {
-      plugins: [
-        tailwind(),
-        autoprefixer(),
-      ],
-    },
-  },
 });
 
 export default overrideVaadinConfig(customConfig);
